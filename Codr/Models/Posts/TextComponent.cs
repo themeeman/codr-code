548f62b882b3bf29d26962b12ba2bfd8a1038784
tomvanwoow <tom@vanhowe.com>
2019-11-21 21:49:55 +0000

    bruh

---
+++ Codr/Models/Posts/TextComponent.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Codr.Models.Posts {
+    public class TextComponent : IEquatable<TextComponent> {
+        public TextComponent() { }
+        public TextComponent(string content, TextStyle style) {
+            Content = content;
+            Style = style;
+        }
+
+        public string Content { get; private set; } = string.Empty;
+        public TextStyle Style { get; private set; } = TextStyle.None;
+
+        public override bool Equals(object? obj) {
+            if (obj is null) return false;
+            return Equals((TextComponent)obj);
+        }
+
+        public bool Equals(TextComponent other) {
+            return Content == other.Content &&
+                   Style == other.Style;
+        }
+
+        public override int GetHashCode() {
+            return HashCode.Combine(Content, Style);
+        }
+
+        public static bool operator ==(TextComponent left, TextComponent right) {
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(TextComponent left, TextComponent right) {
+            return !(left == right);
+        }
+    }
+}
---
+++ Codr/Models/Posts/TextStyle.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Codr.Models.Posts {
+    [Flags]
+    public enum TextStyle {
+        None          = 0x00,
+        Bold          = 0x01,
+        Underlined    = 0x02,
+        Italics       = 0x04,
+        Strikethrough = 0x08,
+        InlineCode    = 0x10,
+        Spoiler       = 0x20,
+    }
+}
