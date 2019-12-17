5b8727a1903eaea39811005bdc9a5a5a838edb13
Thomas Van Howe <tom@vanhowe.com>
2019-12-17 13:59:12 +0000

    hash

---
+++ Codr/Models/HashedPassword.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace Codr.Models {
+    public class HashedPassword {
+        public string Password { get; private set; }
+        public HashedPassword(byte[] unhashedPassword) {
+            // Generate the salt as a byte array using a cryptographic PRNG (different each time)
+            byte[] salt = new byte[16];
+            using (var rng = new RNGCryptoServiceProvider())
+                rng.GetBytes(salt);
+
+            // Generate the hash of the password using SHA 256, a secure hashing algorithm
+            byte[] hash;
+            using (var sha256 = SHA256.Create())
+                hash = sha256.ComputeHash(unhashedPassword);
+
+            // Combines the hash and the salt together for storage
+            byte[] hashBytes = new byte[36];
+            Array.Copy(salt, 0, hashBytes, 0, 16);
+            Array.Copy(hash, 0, hashBytes, 16, 20);
+            // Converts the combined hash and salt to a string
+            Password = Convert.ToBase64String(hashBytes);
+        }
+
+        public override string ToString() {
+            return Password;
+        }
+    }
+}
---
+++ Codr/Models/User.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Codr.Models {
+    public class User : IEquatable<User> {
+        public string Id { get; private set; } = string.Empty;
+        public string Email { get; private set; }
+        public string Password {
+            get => password;
+            set {
+                byte[] salt;
+
+            }
+        }
+
+        public string FirstName { get; set; }
+        public string? LastName { get; set; }
+        public List<string> Posts { get; private set; } = new List<string>();
+        public IEnumerable<string> Friends => friends;
+        private readonly HashSet<string> friends = new HashSet<string>();
+        private string password = string.Empty;
+
+        public User(string email, string password, string firstName, string? lastName = null) {
+            Email = email;
+            Password = password;
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
+        public override bool Equals(object? obj) {
+            if (obj is null || !(obj is User))
+                return false;
+            return Equals((User)obj);
+        }
+
+        public bool Equals(User other) {
+            return Id == other.Id;
+        }
+
+        public override int GetHashCode() {
+            return HashCode.Combine(Id);
+        }
+
+        public static bool operator ==(User left, User right) {
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(User left, User right) {
+            return !(left == right);
+        }
+    }
+}
